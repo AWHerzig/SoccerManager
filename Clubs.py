72b7f3a2401cb405a4052dc9b28fd65970e5bc7a
AWHerzig <109994816+AWHerzig@users.noreply.github.com>
2024-04-25 22:41:22 -0400

    first teams

--- Clubs.py
+++ Clubs.py
@@ -1,4 +1,50 @@
 from Competitions import *
-ENG1 = [
-    
+ENG1 = [  # http://elofootball.com/country.php?countryiso=ENG&season=2023-2024
+    CLUB('Manchester City', 'MNC', 95),  # 2397
+    CLUB('Liverpool', 'LIV', 92),
+    CLUB('Arsenal', 'ARS', 91),
+    CLUB('Aston Villa', 'AST', 87),
+    CLUB('Tottenham Hotspur', 'TOT', 87),
+    CLUB('Manchester United', 'MNU', 84),
+    CLUB('Newcastle United', 'NEW', 82),
+    CLUB('West Ham United', 'WHU', 80),
+    CLUB('Chelsea', 'CHE', 80),
+    CLUB('Brighton HA', 'BHA', 79),
+    CLUB('Brentford', 'BRE', 76),
+    CLUB('Fulham', 'FUL', 76),
+    CLUB('Wolverhampton', 'WOL', 75),
+    CLUB('Crystal Palace', 'PAL', 72),
+    CLUB('Everton', 'EVE', 70),
+    CLUB('Bournemouth', 'BOU', 70),
+    CLUB('Nottingham Forest', 'FOR', 66),
+    CLUB('Burnley', 'BUR', 65),
+    CLUB('Luton Town', 'LUT', 63),
+    CLUB('Sheffield United', 'SHE', 59),  # 1781
+]
+
+ENG2 = [
+    CLUB('Leicester City', 'LEI', 69), # 1927
+    CLUB('Leeds United', 'LEE', 69),
+    CLUB('Ipswich Town', 'IPS', 64),
+    CLUB('Southampton', 'SOU', 64),
+    CLUB('West Brom', 'BRO', 59),
+    CLUB('Norwich City', 'NOR', 60),
+    CLUB('Hull City', 'HUL', 56),
+    CLUB('Coventry', 'COV', 60),
+    CLUB('Middlesbrough', 'MID', 58),
+    CLUB('Preston North End', 'PRE', 55),
+    CLUB('Cardiff City', 'CAR', 51),
+    CLUB('Bristol City', 'BRI', 56),
+    CLUB('Sunderland', 'SUN', 54),
+    CLUB('Swansea City', 'SWA', 56),
+    CLUB('Watford', 'WAT', 54),
+    CLUB('Millwall', 'MIL', 52),
+    CLUB('Stoke City', 'STO', 52),
+    CLUB('Queens Park Rangers', 'QPR', 49),
+    CLUB('Blackburn Rovers', 'BLA', 53),
+    CLUB('Plymouth Argyle', 'PLY', 51),
+    CLUB('Sheffield Wednesday', 'WED', 54),
+    CLUB('Birmingham City', 'BIR', 48),
+    CLUB('Huddersfield Town', 'HUD', 48),
+    CLUB('Rotherham', 'ROT', 40)
 ]
